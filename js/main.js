4218bd4a1d4d0c2bdf4e215334cd1bdafc5443fc
FrankLaurora <francesco.laurora9@gmail.com>
2021-08-30 19:11:24 +0200

    Add workin' buttons. Missing style.

--- js/main.js
+++ js/main.js
@@ -1,24 +1,56 @@
-var btnGenera = document.getElementById("get_Ticket");
+var btnGenera = document.getElementById("get_ticket");
+var btnAnnulla = document.getElementById("erase");
 var checkOut = Number ;
 
 btnGenera.addEventListener("click",
     function() {
-        //raccolgo l'input di "userName" e lo scrivo nell'html
-        var userName = document.getElementById("user_Name").value;
-        document.getElementById("your_Name").innerHTML = userName;
-
+        //raccolgo l'input di "userName"
+        var userName = document.getElementById("user_name").value;
+        
         //raccolgo l'input di "distance" e di "promo"
         var distance = parseInt(document.getElementById("distance").value);
         var discount = document.getElementById("promo").value;
-
+        
         //calcolo il prezzo del biglietto secondo i valori raccolti
-        checkOut = (0.21 * distance).toFixed(2) ;
+        checkOut = (0.21 * distance).toFixed(2);
         if (discount == "minorenne") {
             checkOut = (checkOut * 0.8).toFixed(2);
         } else if (discount ==  "over") {
             checkOut = (checkOut * 0.6).toFixed(2);
         }
+        
+        //genero un numero casuale per la carrozza
+        var yourSeat = Math.floor(Math.random() * 9) + 1;
+        
+        //genero il CP
+        var yourCP = yourSeat.toString() + (Math.floor(Math.random() * (10000 - 1000)) + 1000);
+        
+        //compilo l'html con le informazioni generate
+        document.getElementById("your_name").innerHTML = userName;
+
+        if (discount == "minorenne") {
+            document.getElementById("your_promo").innerHTML = "Young";
+        } else if (discount == "over") {
+            document.getElementById("your_promo").innerHTML = "Silver";
+        } else {
+            document.getElementById("your_promo").innerHTML = "Standard";
+        }
+        
+        document.getElementById("your_seat").innerHTML = yourSeat;
+        
+        document.getElementById("your_CP").innerHTML = yourCP;
+        
+        document.getElementById("your_check_out"). innerHTML = checkOut + "€";
+
+        document.getElementById("your_ticket").classList.add("visible");
     }
 );
 
-
+btnAnnulla.addEventListener("click",
+    function() {
+        var userName = document.getElementById("user_name").value = "";
+        var distance = document.getElementById("distance").value = "";
+        var discount = document.getElementById("promo").value = "";
+        document.getElementById("your_ticket").classList.remove("visible");
+    }
+);
